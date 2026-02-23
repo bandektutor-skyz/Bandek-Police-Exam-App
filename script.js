8e78921ca56a61527715bba2757c6c74006ac7bd
bandektutor-skyz <bandektutor@gmail.com>
2026-02-23 16:23:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,8 @@ currentQuestions = allQuestions.filter(q => Number(q.set) === Number(setNumber))
         document.getElementById('quiz-content').style.display = 'block';
         document.getElementById('timer-container').style.display = 'block';
         showQuestion();
-        startTimer();
+        startTimer();document.getElementById('home-btn').style.display = 'block'; // เพิ่มบรรทัดนี้เพื่อให้ปุ่มโชว์ตอนเริ่มสอบ
+}
     } else {
         alert("ขออภัย! ชุดที่ " + setNumber + " ยังไม่มีข้อสอบ");
     }
